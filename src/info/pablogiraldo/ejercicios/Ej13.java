fcfcd91ef9e2ede54807abb1f1c03dee612bccd1
laborus <saulogiral@gmail.com>
2020-02-17 10:54:18 +0100

    Update

--- src/info/pablogiraldo/ejercicios/Ej13.java
+++ src/info/pablogiraldo/ejercicios/Ej13.java
@@ -1,14 +1,27 @@
 package info.pablogiraldo.ejercicios;
 
+import java.util.Scanner;
+
 public class Ej13 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		String nombreAp = "fulano de tal".toUpperCase();
+		String nombreAp = "";
 
-		String[] nombreSplit = nombreAp.split(" ");
+//		String nombreAp = "fulano de tal".toUpperCase();
+//
+//		String[] nombreSplit = nombreAp.split(" ");
 
 		char inicial = ' ';
+
+		System.out.println("\n");
+		System.out.println("Nombre y apellidos:");
+		nombreAp = sc.nextLine().toUpperCase();
+
+		sc.close();
+
+		String[] nombreSplit = nombreAp.split(" ");
 
 		for (String nomPart : nombreSplit) {
 			inicial = nomPart.charAt(0);
--- src/info/pablogiraldo/ejercicios/Ej18.java
+++ src/info/pablogiraldo/ejercicios/Ej18.java
@@ -9,15 +9,32 @@ public class Ej18 {
 
 		String frase = "";
 
+		String fraseCambiada = "";
+
+		char ch = ' ';
+
 		System.out.println("\n");
 		System.out.println("Introduce una frase:");
 		frase = sc.nextLine();
 
 		sc.close();
 
-		frase = frase.replace('a', 'e');
+//		frase = frase.replace('a', 'e');
+//
+//		System.out.println(frase);
 
-		System.out.println(frase);
+		for (int i = 0; i < frase.length(); i++) {
+			ch = frase.charAt(i);
+
+			if (ch == 'a') {
+				ch = 'e';
+			}
+
+			fraseCambiada += ch;
+
+		}
+
+		System.out.println(fraseCambiada);
 
 	}
 
