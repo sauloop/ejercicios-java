75ad34921a81efa6600888deab58ef4571c9c201
Pablo Giraldo <pablogirald@yahoo.es>
2020-03-04 19:18:43 +0100

    Update

--- src/info/pablogiraldo/clase/Biblioteca.java
+++ src/info/pablogiraldo/clase/Biblioteca.java
@@ -1,14 +1,12 @@
 package info.pablogiraldo.clase;
 
-import java.util.ArrayList;
-
 public class Biblioteca {
 
-	private ArrayList<LibroB> libros;
-
-	public Biblioteca(ArrayList<LibroB> libros) {
-		this.libros = libros;
-	}
+//	private ArrayList<LibroB> libros;
+//
+//	public Biblioteca(ArrayList<LibroB> libros) {
+//		this.libros = libros;
+//	}
 
 //	public boolean prestamo(String titulo) {
 //
