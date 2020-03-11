503ffbd0f72389c17a712066d24d03f6eb425d34
laborus <saulogiral@gmail.com>
2020-03-11 11:30:05 +0100

    Update

--- src/info/pablogiraldo/file/JuegoPreguntas.java
+++ src/info/pablogiraldo/file/JuegoPreguntas.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+//import java.io.IOException;
+//import org.jdom.Attribute;
+//import org.jdom.Document;
+//import org.jdom.Element;
+//import org.jdom.output.Format;
+
 public class JuegoPreguntas {
 
 	private int puntos;
@@ -280,5 +286,44 @@ public class JuegoPreguntas {
 		}
 
 	}
+
+//	public void xmlExport() {
+//
+//		try {
+//
+//			Element company = new Element("company");
+//			Document doc = new Document(company);
+//			doc.setRootElement(company);
+//
+//			Element staff = new Element("staff");
+//			staff.setAttribute(new Attribute("id", "1"));
+//			staff.addContent(new Element("firstname").setText("yong"));
+//			staff.addContent(new Element("lastname").setText("mook kim"));
+//			staff.addContent(new Element("nickname").setText("mkyong"));
+//			staff.addContent(new Element("salary").setText("199999"));
+//
+//			doc.getRootElement().addContent(staff);
+//
+//			Element staff2 = new Element("staff");
+//			staff2.setAttribute(new Attribute("id", "2"));
+//			staff2.addContent(new Element("firstname").setText("low"));
+//			staff2.addContent(new Element("lastname").setText("yin fong"));
+//			staff2.addContent(new Element("nickname").setText("fong fong"));
+//			staff2.addContent(new Element("salary").setText("188888"));
+//
+//			doc.getRootElement().addContent(staff2);
+//
+//			// new XMLOutputter().output(doc, System.out);
+//			XMLOutputter xmlOutput = new XMLOutputter();
+//
+//			// display nice nice
+//			xmlOutput.setFormat(Format.getPrettyFormat());
+//			xmlOutput.output(doc, new FileWriter("c:\\file.xml"));
+//
+//			System.out.println("File Saved!");
+//		} catch (IOException io) {
+//			System.out.println(io.getMessage());
+//		}
+//	}
 
 }
